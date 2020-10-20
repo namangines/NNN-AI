ebe6de0943a47ffaa3a1d9b6f5c2672e16fa6a7a
namangines <56846784+namangines@users.noreply.github.com>
2020-10-20 16:22:26 -0400

    class work
    setting up for the homework. Still need EventManagerDel

---
+++ Assets/Scripts/Misc/DetectForEvents.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class DetectForEvents : MonoBehaviour
+{
+    void OnEnable()
+    {
+        
+    }
+
+    void OnDisable()
+    {
+        
+    }
+
+    void playerHeard()
+    {
+        Debug.Log("Player was heard");
+    }
+
+}
