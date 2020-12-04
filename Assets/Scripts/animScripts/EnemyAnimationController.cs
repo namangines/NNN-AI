1b9e7c7443b5e9357e8d79eecc2c17a35331ae33
NoDienel <46758801+NoDienel@users.noreply.github.com>
2020-12-04 13:19:19 -0500

    AI Animation

---
+++ Assets/Scripts/animScripts/EnemyAnimationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAnimationController : MonoBehaviour
+{
+    public NavMeshAgent VelocityReader;
+    private Animator animator;
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //normalized
+        animator.SetFloat("velocity", 
+            VelocityReader.velocity.magnitude / VelocityReader.desiredVelocity.magnitude);
+    }
+}
